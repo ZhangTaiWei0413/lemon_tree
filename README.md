ce31a4e0102779662dc117e50294c124b9c656f3
張泰瑋 <tlrzhang@gmail.com>
2024-09-21 03:19:06 +0800

    9/20 更新其四:bmi判斷

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ git config --global user.email tlrzhang@gmail.com
 新增script，在script裡打程式or在script後面加src:"檔名.js"
 
 <input type="資料型態:數字,字串..." id="變數名稱">:創建一個擁有變數名稱的輸入欄，但資料型態不一定限制
-<label for="變數名稱">:在擁有該變數名稱的輸入欄前面創建文字
+<label for="變數名稱">:在擁有該變數名稱的物件前面創建文字
 <button onclick="函數名稱()">:創建一個按鍵，並在點擊時激活該函數名稱裡的程式碼
 <p id="變數名稱">:段落，可以簡單理解成把變數名稱的內容顯示出來
+
+Number(document.getElementById("變數名稱").value);
+Number:將字串改為數字
+document:代表整個html物件
+getElementById("變數名稱"):獲取指定變數的元素
+.value:指定要獲取的元素的值，如果不加就會獲得整個元素本身
+.innerHTML:取得或設置指定元素的內容
+.toFixed(x):代表要取到小數點後的x位
+
 寫完程式後點選原始檔控制>填寫訊息>提交>同步變更
--- bmi.js
+++ bmi.js
@@ -1,8 +1,27 @@
 function calcBmi() {
     var weight = Number(document.getElementById("weight").value);
     var height = Number(document.getElementById("height").value);
+    
+    // 計算 BMI
     var bmi = weight / ((height / 100) ** 2);
-    document.getElementById("bmi").innerHTML = bmi;
-    console.log("Hello, Your BMI is:" + bmi);
-}
-IPO
+
+    // 宣告結果變數
+    var judgment = "";
+
+    // 判斷 BMI 狀況
+    if (bmi < 18.5) 
+        {judgment = "You are underweight.";}
+     else if (bmi >= 18.5 && bmi < 24.9) 
+        {judgment = "You are in the normal range.";}
+     else if (bmi >= 25 && bmi < 29.9) 
+        {judgment = "You are overweight.";}
+     else 
+        {judgment = "You are obese.";}
+    
+
+    // 顯示 BMI 值和判斷結果
+    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2) + "." + judgment;
+
+    // 將結果輸出到控制台
+    console.log("Your BMI is: " + bmi.toFixed(2) + ". " + judgment);
+    }
