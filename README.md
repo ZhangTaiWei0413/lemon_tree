ebf25dbad45d2144e09658ec3682daa88f0e84bb
張泰瑋 <tlrzhang@gmail.com>
2024-09-20 15:03:43 +0800

    9/20 更新其三:網頁互動

--- README.md
+++ README.md
@@ -10,4 +10,9 @@ git config --global user.email tlrzhang@gmail.com
 輸入html:5建構網頁基礎
 在head跟body之間
 新增script，在script裡打程式or在script後面加src:"檔名.js"
+
+<input type="資料型態:數字,字串..." id="變數名稱">:創建一個擁有變數名稱的輸入欄，但資料型態不一定限制
+<label for="變數名稱">:在擁有該變數名稱的輸入欄前面創建文字
+<button onclick="函數名稱()">:創建一個按鍵，並在點擊時激活該函數名稱裡的程式碼
+<p id="變數名稱">:段落，可以簡單理解成把變數名稱的內容顯示出來
 寫完程式後點選原始檔控制>填寫訊息>提交>同步變更
--- bmi.js
+++ bmi.js
@@ -1,4 +1,8 @@
-var weight=50;
-var height=160;
-var bmi = weight/((height/100)**2);
-console.log("Hello! Your BMI value is "+ bmi);
+function calcBmi() {
+    var weight = Number(document.getElementById("weight").value);
+    var height = Number(document.getElementById("height").value);
+    var bmi = weight / ((height / 100) ** 2);
+    document.getElementById("bmi").innerHTML = bmi;
+    console.log("Hello, Your BMI is:" + bmi);
+}
+IPO
