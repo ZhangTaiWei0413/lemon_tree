982858e8949270fa5b3810bd97c9a2802fa6cdc5
張泰瑋 <tlrzhang@gmail.com>
2024-10-13 21:37:34 +0800

    結果變成位址或隨機增加

--- 2darray.js
+++ 2darray.js
@@ -30,16 +30,67 @@
 // ary2d[3].push("C");
 //console.log(ary2d)
 
-//Dynamic
-var row=9, col=7;
-var dynaAry2d=[];
-for(var _row=0;_row<row;_row++)
-{
+//使用加法讓陣列內容逐漸變大
+var row = 9, col = 7;//子陣列個數，子陣列內容個數
+var dynaAry2d = [];//主陣列
+var x = 0;//行樹逐漸增加
+
+for (var _row = 0; _row < row; _row++) {
     dynaAry2d.push([]);
-    for(var _col=0;_col<col;_col++){
-        dynaAry2d[_row].push(Number(_col)+Number(x));
+    for (var _col = 0; _col < col; _col++) {
+        dynaAry2d[_row].push(_col + x);//可以改成:dynaAry2d[_row].push(_row * col + _col);
     }
-    x=x+7;
+    x += 7;
 }
-console.log(dynaAry2d);
-//8
+
+console.log("使用加法讓陣列內容逐漸變大");
+console.log(JSON.stringify(dynaAry2d));//使輸出結果不要換行
+
+//使輸出變成在其陣列裡的位址
+var row = 9, col = 7;//子陣列個數，子陣列內容個數
+var dynaAry2d = [];//主陣列
+
+for (var _row = 0; _row < row; _row++) {
+    dynaAry2d.push([]);
+    for (var _col = 0; _col < col; _col++) {
+        dynaAry2d[_row].push(`${_row},${_col}`);
+    }
+}
+
+console.log("使輸出變成在其陣列裡的位址");
+console.log(JSON.stringify(dynaAry2d));//使輸出結果不要換行
+
+//使輸出結果隨機遞增
+var row = 9, col = 7;
+var dynaAry2d = [];
+var baseValue = 0;//隨機累加變數
+
+for (var _row = 0; _row < row; _row++) {
+    dynaAry2d.push([]);
+    for (var _col = 0; _col < col; _col++) {
+        var randomIncrement = Math.floor(Math.random() * 5); // 生成0到4的隨機增量
+        baseValue += randomIncrement; // 在基礎值上增加隨機增量
+        dynaAry2d[_row].push(baseValue); // 把結果放入二維陣列中
+    }
+}
+
+console.log("使輸出結果隨機遞增");
+console.log(JSON.stringify(dynaAry2d));
+
+//使輸出內容個數隨機
+var row = 9;
+var dynaAry2d = [];
+var currentValue = 0; //累加變數
+
+for (var _row = 0; _row < row; _row++) {
+    var randomCol = Math.floor(Math.random() * 10) + 1; 
+    dynaAry2d.push([]);
+    for (var _col = 0; _col < randomCol; _col++) {
+        dynaAry2d[_row].push(currentValue);
+        currentValue++; // 變數內容遞增
+    }
+}
+
+console.log("使輸出內容個數隨機");
+console.log(JSON.stringify(dynaAry2d));
+
