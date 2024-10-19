c3b8792389d4921f4f294cb3f2d0253607740eb8
張泰瑋 <tlrzhang@gmail.com>
2024-10-20 03:32:36 +0800

    2dary4html完成

--- 2dAry4html.js
+++ 2dAry4html.js
@@ -1,9 +1,9 @@
 var ary2d=[
-    [1,1,0,1,1], //index 0
-    [1,0,1,1,0], //index 1
-    [1,0,1,0,0], //index 2
-    [1,0,1,1,0], //index 3
-    [1,0,1,0,0]   //index 4
+    [1,1,0,1,1,1], //index 0
+    [1,0,1,1,0,0], //index 1
+    [1,0,1,0,0,1], //index 2
+    [1,0,1,1,0,0], //index 3
+    [1,0,1,0,0,1]   //index 4
 ];
 
 var row=ary2d.length;
@@ -11,13 +11,15 @@ var col=ary2d[0].length;
 
 //draw map
 var canvas = document.getElementById("map").getContext("2d");
-var size = Math.min(canvas.canvas.width / row, canvas.canvas.height / col);
+var size=Math.min(canvas.canvas.height/row, canvas.canvas.width/col);
 //var size=canvas.width/row
 
-{for(var _row=0;_row<row;_row++)
-    {for(var _col=0;_col<col;_col++)
+for(var _row=0;_row<row;_row++)
+    {
+        for(var _col=0;_col<col;_col++)
+        {
         //ar2d[_row][_col]=>0,1
-        if(ary2d[_col][_row]==1){
+        if(ary2d[_row][_col]==1){
             canvas.fillStyle="#ffffff"
         }else{
             canvas.fillStyle="#000000"
@@ -25,5 +27,6 @@ var size = Math.min(canvas.canvas.width / row, canvas.canvas.height / col);
         //600/5=>120  coordinate, width, heigth
         canvas.fillRect(_col*size,_row*size,size,size);
         canvas.strokeRect(_col*size,_row*size,size,size);
+        }
     }
-}
+
